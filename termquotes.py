68c0bb6a7efbac046c9c10b50dd5ac6caae6af6d
Haukur Páll Hallvarðsson <haukur@outoftheblue.is>
2012-01-26 19:08:21 +0000

    Fully functional BUT Needs debugging to implement commented code

--- termquotes.py
+++ termquotes.py
@@ -5,26 +5,58 @@ import os
 import sys
 from random import choice
 
-# NOTE Use ConfigParser to find quote files.
+# TODO Use ConfigParser to find quote files instead of this hardcoded crap.
 current_dir = os.path.dirname(sys.argv[0])
-files = ['%s/icelandic.txt' % current_dir, '%s/general.txt' % current_dir,
-         '%s/movies.txt' % current_dir]
+#files = ['%s/icelandic.txt' % current_dir, '%s/general.txt' % current_dir,
+#         '%s/movies.txt' % current_dir]
+files = ['%s/icelandic.txt' % current_dir]
+
+file_len = lambda file: sum(1 for _ in open(file))
 
 def parse(input_file):
     '''Return quotes from a parsed file.'''
     quotes = {}
     lines = []
     temp_quotes = []
+    len = file_len(input_file)
+    # TODO Debug the following code and replace the old one. Raises IndexError.
+    # NOTE This code makes the "last-line-empty" requirement (for the quote
+    #      files) unnecessary.
+    '''
     with open(input_file) as file:
-        # Find the length of the the file (in number of lines).
-        # Used later to detect EOF. NOTE Find a better and more pythonic way.
-        len = sum([1 for line in file])
+        for line in enumerate(file):
+            num, text = line
+            if text[0] == '#':
+                # The line contains a comment: ignore it.
+                continue
+            if text[0] == '[' and text[-2] == ']':
+                # The line contains the name of an author: add him.
+                author = text[1:-2]
+                continue
+            if num == len:
+                if text != '\n':
+                    lines.append(text)
+            if text in ['&\n', '\n'] or num == len:
+                # Ampersand and/or newline: add last quote to temp_quotes.
+                temp_quotes.append(''.join(lines))
+                # Clear lines for the next quote whether there is one or not.
+                lines = []
+                if line == '\n' or num == len:
+                    # Finish adding the quotes if last quote from author or end
+                    # of file.
+                    quotes[author] = temp_quotes
+                    # Clear temp_quotes lest we're not at the last author.
+                    temp_quotes = []
+                continue
+            # Add the text to lines.
+            lines.append(text)
+    '''
     file = open(input_file)
     index = 0
     for line in file:
         index += 1
         if line[0] == '#':
-            # The line contains a comment: ignore it.
+            # The line contains a comment - ignore it. No special reason ...
             continue
         elif line[0] == '[' and line[-2] == ']':
             # Line contains the name of the author.
@@ -47,12 +79,18 @@ def parse(input_file):
 
 def main():
     '''Print a randomly selected quote.'''
+    # TODO
+    # - Add options and use optparser to allow users to print a quote
+    #   on-demand.
+    # - Add some kind of formatting options - allow users to select different
+    #   settings regarding how the output should be printed.
+    # - Write a function to scrape websites for quotes and another to save
+    #   them.
     quotes = {}
     for file in files:
-        # Update the quotes dictionary with quotes from the chosen files.
+        # Load all the quotes.
         quotes.update(parse(file))
-    # Choose an author based on the number of quotes by him (more or less the
-    # same probability for all the quotes to appear. NOTE Do it some other way.
+    # Choose an author based on the number of quotes by him.
     author = choice([key for key in quotes for value in quotes[key]])
     # Select a quote from the author chosen above.
     quote = choice(quotes[author])
